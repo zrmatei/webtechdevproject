8f0111b498508e46c2b2542d2af68a19ad911975
zrmatei <149681533+zrmatei@users.noreply.github.com>
2025-01-19 21:13:07 +0200

    Add files via upload

--- frontendareala/src/App.js
+++ frontendareala/src/App.js
@@ -19,7 +19,7 @@ function App() {
         }
 
         try {
-            const response = await fetch('http://localhost:3001/getActivity', {
+            const response = await fetch('https://webtechdevproject.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
